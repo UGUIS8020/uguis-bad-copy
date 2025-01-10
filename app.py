68cf7a36f155a2723311fa3ea263c9577128a316
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-01-10 16:51:11 +0900

    新越谷

--- app.py
+++ app.py
@@ -714,7 +714,7 @@ def get_schedules_with_formatting():
             active_schedules,
             key=lambda x: x.get('date', ''),
             reverse=False
-        )[:10]  # 最新10件を取得
+        )[:12]  # 最新12件を取得
         
         # 以下は既存の処理をそのまま維持
         unique_user_ids = set()
